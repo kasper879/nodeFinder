2c6507802a9a93bc1e9f7f577a9bc16c2de21374
kasper879 <kasper8791@gmail.com>
2017-12-20 16:23:07 +0100

    comit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+ var PORT = process.env.PORT || 3000
 app.get('/', (req, res) => res.send('Node finder'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
